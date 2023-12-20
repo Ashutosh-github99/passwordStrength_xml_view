84919fd48e072f3dc60256bbd9b3f9cb3608eeac
Ashutosh_pattanaik_isu <ashutosh.p@iserveu.co.in>
2023-12-20 17:02:09 +0530

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+C
+settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+p
+@app/src/main/java/com/challange/passwordstrength/MainActivity.kt,9\8\989c1bf3edcc9b4f5ca87cac92b1dd1845a63c89
---
+++ app/src/main/java/com/challange/passwordstrength/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.challange.passwordstrength
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
+import com.challange.passwordstrength.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawableSuccess = ContextCompat.getDrawable(this,R.drawable.ic_have)
+        val drawableError = ContextCompat.getDrawable(this,R.drawable.ic_error)
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                s?.let {password->
+
+                    if (password.isNotEmpty())
+                    {
+                        val strength = checkPasswordStrength(
+                            password.toString(),
+                            drawableSuccess,
+                            drawableError
+                        )
+                        //update ui
+                        binding.tvResult.text = "Password Strength : $strength"
+                    }else{
+                        resetDrawables()
+                        binding.tvLength.text="00"
+                        binding.tvResult.text=""
+                    }
+
+                }
+            }
+        }
+
+        binding.etPassword.addTextChangedListener(textWatcher)
+    }
+
+    private fun checkPasswordStrength(password: String, drawableSuccess: Drawable?, drawableError: Drawable?): Any {
+
+        val length = password.length
+
+        var hasLowerCase = false
+        var hasUpperCase = false
+        var hasSymbol = false
+
+        binding.tvLength.text = String.format("%02d",length)
+        for (char in password){
+
+            if (!hasLowerCase && !hasUpperCase && !hasSymbol){
+                resetDrawables()
+            }
+             if (Character.isLowerCase(char)){
+                 hasLowerCase = true
+                 binding.hasLowerCase.setCompoundDrawablesWithIntrinsicBounds(drawableSuccess,null,null,null)
+
+             }
+            if (Character.isUpperCase(char)){
+                hasUpperCase = true
+                binding.hasUpperCase.setCompoundDrawablesWithIntrinsicBounds(drawableSuccess,null,null,null)
+
+            }
+            if (!char.isLetterOrDigit()){
+                hasSymbol = true
+                binding.hasSymbol.setCompoundDrawablesWithIntrinsicBounds(drawableSuccess,null,null,null)
+            }
+        }
+
+        if (length< 8 || !(hasLowerCase && hasUpperCase && hasSymbol)){
+
+            return  "weak"
+        }
+        else if (length < 12){
+            return  "Modorate"
+        }
+        else {
+            return  "Strong"
+        }
+    }
+
+    private fun resetDrawables() {
+        val drawbleError = ContextCompat.getDrawable(this,R.drawable.ic_error)
+
+        binding.hasLowerCase.setCompoundDrawablesWithIntrinsicBounds(drawbleError,null,null,null)
+        binding.hasSymbol.setCompoundDrawablesWithIntrinsicBounds(drawbleError,null,null,null)
+        binding.hasUpperCase.setCompoundDrawablesWithIntrinsicBounds(drawbleError,null,null,null)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
